1159415b05cbc31b892fc7d94c493f08194f42aa
Aaron <mohammad.chegini@hotmail.com>
2020-08-07 22:23:40 +0430

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Student
+{
+    public int studentID {get; set;}
+    public string Name { get; set; }
+    public void setStudentID(){
+        string filename = "currID.txt";
+        int currID = Convert.ToInt32(File.ReadAllText(filename));
+        Console.WriteLine(currID);
+    }
+}
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "text.txt";
+            string[] lines = File.ReadAllLines( filename );
+            foreach ( string line in lines )
+            {
+                string[] col = line.Split(',');
+                foreach (var item in col)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("\n\n\n\n\n");
+            }
+        }
+    }
+}
