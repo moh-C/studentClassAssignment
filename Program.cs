2d72b37179cd223af067a163d6b588f81cc5c213
Aaron <mohammad.chegini@hotmail.com>
2020-08-07 22:32:43 +0430

    Use case of static members in C#

--- Program.cs
+++ Program.cs
@@ -1,34 +1,26 @@
 ﻿using System;
 using System.IO;
 
-class Student
-{
-    public int studentID {get; set;}
-    public string Name { get; set; }
-    public void setStudentID(){
-        string filename = "currID.txt";
-        int currID = Convert.ToInt32(File.ReadAllText(filename));
-        Console.WriteLine(currID);
-    }
-}
-
 namespace myapp
 {
+    class Person
+    {
+        public string Name;
+        public void Introduce(string to) {
+            Console.WriteLine("Hello {0}, my name is {1}!", to, this.Name);
+        }
+
+        public static Person Parse(string someName){
+            var person = new Person();
+            person.Name = someName;
+            return person;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string filename = "text.txt";
-            string[] lines = File.ReadAllLines( filename );
-            foreach ( string line in lines )
-            {
-                string[] col = line.Split(',');
-                foreach (var item in col)
-                {
-                    Console.WriteLine(item);
-                }
-                Console.WriteLine("\n\n\n\n\n");
-            }
+            Person.Parse("Mammad").Introduce("Ali");
         }
     }
 }
